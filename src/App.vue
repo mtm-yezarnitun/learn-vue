848d9708a06ad40df32007df8fd71c6a7bedf0a2
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-10 09:50:48 +0630

    [add] simple counter

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>Vue Counter</h1>
+    <h2>{{ count }}</h2>
+    <button @click="increase">+ Add</button>
+    <button @click="reset"> * Reset * </button>
+    <button @click="decrease">- Subtract</button>
+    <input type="number" v-model.number="incrementAdd"></input>
+    <button @click="addIncrement"> Add to increase </button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const incrementAdd = ref(1)
+
+function increase() {
+  count.value++
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+function decrease() {
+  if (count.value > 0) {
+    count.value--
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+function reset() {
+  count.value = 0
+}
+
+function addIncrement() {
+  count.value += incrementAdd.value
+}
+
+</script>
+
+<style>
+.container {
+  text-align: center;
+  padding-top: 50px;
+}
+button {
+  margin: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
 }
 </style>
