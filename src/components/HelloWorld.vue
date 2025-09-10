98139ae6c8265754e5ba540a70ff60a501a242be
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-10 09:06:34 +0630

    - Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    };
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <input v-model="message" />
+  </div>
+</template>
+
+
