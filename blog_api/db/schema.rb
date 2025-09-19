b6abed61585475c097bb018c22f88ba573de1629
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-19 16:48:23 +0630

    [add] col: name to users

--- blog_api/db/schema.rb
+++ blog_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_19_043734) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_19_093955) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_19_043734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
