02af9a837610992b82d354ee530184f6a8484b1d
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-23 14:29:25 +0630

    [add] login/register rspec

---
+++ blog_api/spec/requests/users/registrations_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "User Registration", type: :request do
+  
+  #post
+  describe "POST /signup" do
+    let(:valid_params) do  {
+        user: {
+          name: "Tester",
+          email: "test@example.com",
+          password: "password123",
+        }
+      }
+    end
+
+    let(:invalid_params) do {
+        user: {
+          name: "",
+          email: "bad-email",
+          password: "short",
+        }
+      }
+    end
+
+    it "registers a new user with valid params" do
+      expect {
+        post "/signup", params: valid_params
+      }.to change(User, :count).by(1)
+
+      expect(response).to have_http_status(:created)
+
+      json = JSON.parse(response.body)
+      expect(json["message"]).to eq("Signed up successfully.")
+      expect(json["user"]["email"]).to eq("test@example.com")
+      expect(json["token"]).to be_present
+    end
+
+    it "does not register with invalid params" do
+      expect {
+        post "/signup", params: invalid_params
+      }.not_to change(User, :count)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      json = JSON.parse(response.body)
+      expect(json["errors"]).to be_present
+    end
+  end
+end
---
+++ blog_api/spec/requests/users/sessions_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Login Sessions", type: :request do 
+  let!(:user) { User.create!(email: "user@example.com", password: "password123") }
+
+    #post
+    describe "POST /login" do
+      let(:valid_params) { { user: { email: user.email, password: "password123" } } }
+      let(:invalid_params)  { { user: { email: user.email, password: "wrongpass" } } }
+      
+
+      it "login with valid credentials" do 
+        post "/login", params: valid_params
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json["message"]).to eq("Logged in successfully.")
+        expect(json["token"]).to be_present
+        expect(json["user"]["email"]).to eq(user.email)
+        end
+
+      it "fails with invalid credentials" do
+        post "/login", params: invalid_params
+        expect(response).to have_http_status(:unauthorized)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to include("Invalid Email or password")
+        end
+    end
+
+    #delete
+    describe "DELETE /logout" do
+        it "logs out the user" do
+        post "/login", params: { user: { email: user.email, password: "password123" } }
+        token = JSON.parse(response.body)["token"]
+        delete "/logout", headers: { "Authorization" => "Bearer #{token}" }
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json["message"]).to eq("Logged out successfully.")
+        end
+    end
+end
